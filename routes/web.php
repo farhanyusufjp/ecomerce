8d9ac9b1ef44360303d15717c7cce0402b71e7e9
JP <farhanyusuf81@gmail.com>
2020-09-13 14:22:23 +0700

    coba

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::get('experience','pagecontroller@experience');
 Route::get('contact','pagecontroller@contact');
 Route::get('partners','pagecontroller@partners');
 
-
+Route::get('partners','pagecontroller@partners');
 // Route::get('about', 'pagecontroller@about');
 
 
